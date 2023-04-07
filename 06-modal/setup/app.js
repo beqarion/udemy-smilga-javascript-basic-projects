20a873e054e620659acb323bb1f59ed7b785c418
Bekarion <beqarioni@yahoo.co.uk>
2023-04-08 03:25:35 +0400

    ii've done modal proj

--- 06-modal/setup/app.js
+++ 06-modal/setup/app.js
@@ -1,4 +1,13 @@
 // select modal-btn,modal-overlay,close-btn
+const modalBtn = document.querySelector(".modal-btn")
+const modalOverlay = document.querySelector(".modal-overlay")
+const closeBtn = document.querySelector(".close-btn")
 // listen for click events on modal-btn and close-btn
 // when user clicks modal-btn add .open-modal to modal-overlay
 // when user clicks close-btn remove .open-modal from modal-overlay
+modalBtn.addEventListener("click", function () {
+  modalOverlay.classList.add("open-modal")
+})
+closeBtn.addEventListener("click", function () {
+  modalOverlay.classList.remove("open-modal")
+})
