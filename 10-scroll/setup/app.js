2b18f715b7a4aaec03375967eb94262525b61951
beqarion <beqarioni@yahoo.co.uk>
2023-05-05 19:17:06 +0400

    beqa

--- 10-scroll/setup/app.js
+++ 10-scroll/setup/app.js
@@ -4,10 +4,61 @@
 //offsetTop - A Number, representing the top position of the element, in pixels
 
 // ********** set date ************
-
+const date = document.querySelector(".date")
+date.innerHTML = new Date().getFullYear()
 // ********** close links ************
+const navToggle = document.querySelector(".nav-toggle")
+const linksContainer = document.querySelector(".links-container")
+const links = document.querySelector(".links")
 
+navToggle.addEventListener("click", function () {
+  // linksContainer.classList.toggle("show-links")
+
+  const containerHeight = linksContainer.getBoundingClientRect().height
+  const linksHeight = links.getBoundingClientRect().height
+  console.log(linksHeight)
+  if (containerHeight == 0) {
+    linksContainer.style.height = `${linksHeight}px`
+  } else {
+    linksContainer.style.height = 0
+  }
+})
 // ********** fixed navbar ************
+const navbar = document.getElementById("nav")
+const topLink = document.querySelector(".top-link")
 
+window.addEventListener("scroll", function () {
+  const scrollHeight = window.scrollY
+  const navbarHeight = navbar.getBoundingClientRect().height
+
+  if (scrollHeight > navbarHeight) {
+    navbar.classList.add("fixed-nav")
+    console.log(navbar)
+  } else {
+    navbar.classList.remove("fixed-nav")
+  }
+
+  if (scrollHeight > this.window.innerHeight) {
+    topLink.classList.add("show-link")
+  } else {
+    topLink.classList.remove("show-link")
+  }
+})
 // ********** smooth scroll ************
 // select links
+const scrollLinks = document.querySelectorAll(".scroll-link")
+scrollLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault()
+    //navigate to specifit spot
+    // const id = e.currentTarget.getAttribute("href").split("#")[1]
+    const id = e.currentTarget.getAttribute("href").slice(1)
+    const element = document.getElementById(id)
+    let position = element.offsetTop
+    window.scrollTo({
+      left: 0,
+      top: position,
+    })
+    linksContainer.style.height = 0
+  })
+})
