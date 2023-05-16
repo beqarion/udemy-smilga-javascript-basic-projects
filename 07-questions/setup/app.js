8a49a7d721d8e3cad963fc2ae54e994f042e7aa6
Bekarion <beqarioni@yahoo.co.uk>
2023-05-17 01:16:54 +0400

    questions added

--- 07-questions/setup/app.js
+++ 07-questions/setup/app.js
@@ -1,2 +1,27 @@
 //using selectors inside the element
+
+// const questions = document.querySelectorAll(".question")
+
+// questions.forEach(function (q) {
+//   const btn = q.querySelector(".question-btn")
+//   btn.addEventListener("click", function (event) {
+//     questions.forEach(function (item) {
+//       if(item !== q){
+//         item.classList.remove("show-text")
+//       }
+//     })
+//     q.classList.toggle("show-text")
+//   })
+// })
+
 // traversing the dom
+
+const btns = document.querySelectorAll(".question-btn")
+
+btns.forEach(function (btn) {
+  btn.addEventListener("click", function (event) {
+    event.currentTarget.parentElement.parentElement.classList.toggle(
+      "show-text"
+    )
+  })
+})
