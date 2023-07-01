c0653be447e46721ebc4bcf76ababce2f8eb89bd
Bekarion <beqarioni@yahoo.co.uk>
2023-07-01 17:10:04 +0400

    numbers done

--- 18-numbers/starter/app.js
+++ 18-numbers/starter/app.js
@@ -1,1 +1,20 @@
-console.log('numbers project');
+const numbers = [...document.querySelectorAll(".number")]
+
+const updateCount = (el) => {
+  const value = parseInt(el.dataset.value)
+  const increment = Math.ceil(value / 1000)
+  let initialValue = 0
+
+  const increaseValue = setInterval(() => {
+    initialValue += increment
+    if (initialValue > value) {
+      el.textContent = `${value}+`
+      return
+    }
+    el.textContent = `${initialValue}+`
+  }, 1)
+}
+numbers.forEach((el) => {
+  console.log(el)
+  updateCount(el)
+})
