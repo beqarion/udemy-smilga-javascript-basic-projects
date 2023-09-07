dd2bfdd12ff058b8a9f03285369c048611d410d5
beqarion <beqarioni@yahoo.co.uk>
2023-09-07 16:35:34 +0400

    workin on dadsjokeapp

--- 21-dad-jokes/starter/app.js
+++ 21-dad-jokes/starter/app.js
@@ -1,1 +1,28 @@
-console.log('Dad Jokes Starter');
+const url = "https://icanhazdadjoke.com/"
+
+const btn = document.querySelector(".btn")
+const result = document.querySelector(".result")
+
+btn.addEventListener("click", () => {
+  result.innerHTML = "asfasdf"
+})
+
+const fetchDadJoke = async () => {
+  const response = await fetch(url, {
+    headers: {
+      Accept: "application/json",
+      "User-Agent": "learning app",
+    },
+  })
+  const data = await response.json()
+}
+
+fetch(url, {
+  headers: {
+    Accept: "application/json",
+  },
+})
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data)
+  })
