bda42583c6044acf7341b110c363d3270851c044
Bekarion <beqarioni@yahoo.co.uk>
2023-12-05 22:28:00 +0400

    wiki-url at the end

--- 28-wikipedia/starter/app.js
+++ 28-wikipedia/starter/app.js
@@ -1,1 +1,51 @@
-console.log('wiki starter');
+const url =
+  "https://en.wikipedia.org/w/api.php?action=query&list=search&srlimit=20&format=json&origin=*&srsearch="
+
+const formDOM = document.querySelector(".form")
+const inputDOM = document.querySelector(".form-input")
+const resultDOM = document.querySelector(".results")
+
+formDOM.addEventListener("submit", (event) => {
+  event.preventDefault()
+  const value = inputDOM.value
+  if (!value) {
+    resultDOM.innerHTML = `<div class='error'>please enter valid search term</div>`
+    return
+  }
+  fetchPages(value)
+})
+
+const fetchPages = async (searchValue) => {
+  resultDOM.innerHTML = `<div class='loading'></div>`
+  try {
+    const response = await fetch(`${url}${searchValue}`)
+    const data = await response.json()
+    const results = data.query.search
+
+    if (results.length < 1) {
+      resultDOM.innerHTML = `<div class='error'>No matching results. Please try again</div>`
+    }
+
+    renderResults(results)
+  } catch (error) {
+    resultDOM.innerHTML = `<div class='error'>there was an error</div>`
+  }
+}
+
+const renderResults = (list) => {
+  const cardsList = list
+    .map((item) => {
+      const { title, snippet, pageid } = item
+      return `<a href=http://en.wikipedia.org/?curid=${pageid};>
+    <h4>${title}</h4>
+    <p>
+      ${snippet}
+    </p>
+  </a>`
+    })
+    .join("")
+
+  resultDOM.innerHTML = `<div class="articles">
+  ${cardsList}
+  </div>`
+}
