ed12a391c820ef04be6c9080e7a6f8928e95d30d
beqarion <beqarioni@yahoo.co.uk>
2023-12-13 17:14:03 +0400

    416th video finished

--- 29-comfy-store/starter/index.js
+++ 29-comfy-store/starter/index.js
@@ -1,9 +1,20 @@
 // global imports
-import './src/toggleSidebar.js';
-import './src/cart/toggleCart.js';
-import './src/cart/setupCart.js';
+import "./src/toggleSidebar.js"
+import "./src/cart/toggleCart.js"
+import "./src/cart/setupCart.js"
 // specific imports
-import fetchProducts from './src/fetchProducts.js';
-import { setupStore, store } from './src/store.js';
-import display from './src/displayProducts.js';
-import { getElement } from './src/utils.js';
+import fetchProducts from "./src/fetchProducts.js"
+import { setupStore, store } from "./src/store.js"
+import display from "./src/displayProducts.js"
+import { getElement } from "./src/utils.js"
+
+const init = async () => {
+  const products = await fetchProducts()
+  if (products) {
+    // add products to the store
+    setupStore(products)
+    console.log(store)
+  }
+}
+
+window.addEventListener("DOMContentLoaded", init)
--- 29-comfy-store/starter/src/store.js
+++ 29-comfy-store/starter/src/store.js
@@ -1,20 +1,24 @@
-import { getStorageItem, setStorageItem } from './utils.js';
-let store = getStorageItem('store');
+import { getStorageItem, setStorageItem } from "./utils.js"
+let store = []
 const setupStore = (products) => {
   store = products.map((product) => {
     const {
       id,
-      fields: { featured, name, price, company, colors, image: img },
-    } = product;
-    const image = img[0].thumbnails.large.url;
-    return { id, featured, name, price, company, colors, image };
-  });
-  setStorageItem('store', store);
-};
+      fields: {
+        featured,
+        name,
+        price,
+        company,
+        colors,
+        image: img,
+        thumbnails,
+      },
+    } = product
 
-const findProduct = (id) => {
-  let product = store.find((product) => product.id === id);
-  return product;
-};
+    const image = img[0].thumbnails.large.url
+    return { id, featured, name, price, company, colors, image }
+  })
+}
+const findProduct = () => {}
 
-export { store, setupStore, findProduct };
+export { store, setupStore, findProduct }
